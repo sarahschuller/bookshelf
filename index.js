8e57b3a051b73391572c2f61c488c8e1137da029
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-05-29 14:15:46 -0400

    create git ignore, add GET endpoints to index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+// Mock Data
+let topBooks = [
+    {
+      title: 'Harry Potter and the Sorcerer\'s Stone',
+      author: 'J.K. Rowling'
+    },
+    {
+      title: 'Lord of the Rings',
+      author: 'J.R.R. Tolkien'
+    },
+    {
+      title: 'Twilight',
+      author: 'Stephanie Meyer'
+    }
+  ];
+
+// GET requests
+app.get('/', (req, res) => {
+    res.send('Welcome to Bookshelf!');
+});
+
+app.get('/documentation', (req, res) => {
+    res.sendFile('public/documentation.html', 
+    { root:__dirname});
+});
+
+app.get('/books', (req, res) => {
+    res.json(topBooks);
+});
+
+// listen for requests
+app.listen(1313, () => {
+    console.log('App is listening on port 1313');
+});
