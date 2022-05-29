b8fba0687b513b40c7db787862e863385f51f0da
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-05-29 12:55:11 -0400

    create files for new server

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello Node!\n');
+}).listen(1313);
+
+console.log('The server is running on lucky Port number 1313!')
